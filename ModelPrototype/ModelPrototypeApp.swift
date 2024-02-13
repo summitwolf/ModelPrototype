e46085057f5bf85953fd72e7cd9afcd10e5b9d6a
Franklin Whitehurst <summitwolf@me.com>
2024-02-13 14:39:21 -0700

    Initial Commit

---
+++ ModelPrototype/ModelPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelPrototypeApp.swift
+//  ModelPrototype
+//
+//  Created by Franklin Whitehurst on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ModelPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
